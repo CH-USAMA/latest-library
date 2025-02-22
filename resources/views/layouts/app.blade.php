3f34f12d6b6b8f590e7c548fcdf9d89c39c2a416
Shusant Tamang <shusanttamang@outlook.com>
2025-02-22 13:39:15 +0000

    created controller for assignment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@ if (document.documentElement) {
                 <!--begin::Mobile logo-->
                 <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
                     <a href="index.html" class="d-lg-none">
-                        <img alt="Logo" src="assets/media/logos/logo.png" class="h-25px"/>
+                        <img alt="Logo" src="assets/media/logos/library.png" class="h-25px"/>
                     </a>
                 </div>
                 <!--end::Mobile logo-->
